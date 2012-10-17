cf9938da4e5839936a2ef082a1471f05ddfddf93
Russell Hay <me@russellhay.com>
2012-10-16 22:08:59 -0700

    Hook up "uploadify" to the page.  Everything is working on the front end.  Need to implement the actual saving of the files to the server before "front-end" is completely done.

---
+++ dropbox_shortcode.php
@@ -0,0 +1,50 @@
+<?php
+
+function dropbox_music_shortcode($atts) {
+    $swf_path = plugins_url('wp-dropbox-music/assets/uploadify.swf');
+    $upload_path = plugins_url('wp-dropbox-music/upload.php');
+?>
+<!-- This is hacky, but wordpress doesn't use a good version of jquery so I can't use wp_enqueue_script -->
+<script src="http://code.jquery.com/jquery-1.7.2.min.js"></script>
+<script src="<?php echo plugins_url('wp-dropbox-music/js/jquery.uploadify-3.1.js'); ?>"></script>
+<script>
+    function append_from_data() {
+        var username = $("#name").val();
+        var email = $("#email").val();
+        
+        $('#file_upload').uploadify('settings', 'formData', {
+            'name': username,
+            'email': email
+        });
+    }
+    
+    $(document).ready(function() {
+        $('#file_upload').hide();
+        $('.required_field focus').focus();
+        $('.required_field').change(function(evt) {
+            var result = true;
+            $.each($('.required_field'), function(idx, tag) {
+                var ele = $(tag);
+                console.log(ele.val());
+                result &= ele.val() != ""; 
+            });
+            var upload_field = $('#file_upload');
+            if (result) {
+                upload_field.uploadify({
+                    'swf': '<?php echo $swf_path; ?>',
+                    'uploader': '<?php echo $upload_path; ?>',
+                    'onUploadStart': append_from_data
+                 });
+            } else {
+                upload_field.hide();
+            }
+        })
+        
+    });
+</script>
+<p><span class="form_label">Name:</span> <input class="required_field focus" type="text" name="name" id="name" /></p>
+<p><span class="form_label">Email Address: </span><input class="required_field" type="text" name="email" id="email" /></p>
+<input type="file" name="file_upload" id="file_upload" />
+<?php
+}
+?>
--- wp-dropbox-music.php
+++ wp-dropbox-music.php
@@ -10,7 +10,17 @@ License: BSD
 */
 
 require_once('dropbox_admin.php');
+require_once('dropbox_shortcode.php');
 
-add_action('admin_menu', 'dropbox_music_admin_menu');
 add_action('admin_init', 'dropbox_music_admin_init');
+add_action('init', 'dropbox_init');
+
+function dropbox_init() {
+    add_shortcode('dropbox-music', 'dropbox_music_shortcode');
+    add_action('admin_menu', 'dropbox_music_admin_menu');
+    
+    wp_register_style('uploadify_css', plugins_url('wp-dropbox-music/css/uploadify.css'));
+    wp_enqueue_style('uploadify_css');
+
+}
 ?>
